b4b40d3986288def10ae7cd89d54b27676dcc95f
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-13 19:41:13 +0200

    Refactored Image enum

--- PathFinder/Custom Classes/Image.swift
+++ PathFinder/Custom Classes/Image.swift
@@ -8,26 +8,18 @@
 
 import UIKit
 
-class Image: UIImageView {
-    
-    init(image: Images) {
-        super.init(frame: .zero)
-        self.image = image.getImage()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-}
-
-enum Images {
+enum Image {
     
     case map
+    case location
+    case results
     
-    func getImage() -> UIImage {
+    func getImage(_ size: UIImage.SymbolScale) -> UIImage {
+        let configuration = UIImage.SymbolConfiguration(pointSize: 16, weight: .semibold)
         switch self {
-        case .map: return UIImage(named: "map") ?? UIImage()
+        case .map: return UIImage(systemName: "map", withConfiguration: configuration) ?? UIImage()
+        case .location: return UIImage(systemName: "location", withConfiguration: configuration) ?? UIImage()
+        case .results: return UIImage(systemName: "magnifyingglass", withConfiguration: configuration) ?? UIImage()
         }
     }
 }
