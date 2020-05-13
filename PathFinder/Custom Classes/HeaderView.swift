661fbad7ec4832024a2ca07cf4ce499f857c80e2
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-13 19:45:23 +0200

    Custom class - HeaderView

---
+++ PathFinder/Custom Classes/HeaderView.swift
@@ -0,0 +1,50 @@
+//
+//  HeaderView.swift
+//  PathFinder
+//
+//  Created by Timothy Stokarski on 13/05/2020.
+//  Copyright © 2020 Timothy Stokarski. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+    
+    private var iconView = IconView()
+    private var titleLabel = Label(font: .systemFont(ofSize: 16, weight: .bold), textColor: .flatBlack, alignment: .left)
+    
+    init(icon: Image, title: String) {
+        self.iconView.imageView.image = icon.getImage(.small)
+        self.titleLabel.text = title
+        super.init(frame: .zero)
+        addSubviews(iconView, titleLabel)
+        setupLayout()
+        setupBackground()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupLayout() {
+        iconView.snp.makeConstraints { (make) in
+            make.height.width.equalTo(60)
+            make.leading.equalToSuperview().offset(-16)
+            make.centerY.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints { (make) in
+            make.leading.equalTo(iconView.snp.trailing).offset(8)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    func setupBackground() {
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 16
+        self.layer.shadowColor = UIColor.flatBlack.cgColor
+        self.layer.shadowOffset = CGSize(width: 4, height: 4)
+        self.layer.shadowRadius = 4
+        self.layer.shadowOpacity = 0.2
+    }
+    
+}
