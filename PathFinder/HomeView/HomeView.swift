fc3bc593b55f8b3793c9eef2ca09a829fa0a391b
Timothy Stokarski <timothy.stokarski@gmail.com>
2020-05-13 20:56:10 +0200

    New Layout design

--- PathFinder/HomeView/HomeView.swift
+++ PathFinder/HomeView/HomeView.swift
@@ -14,49 +14,38 @@ import SnapKit
 class HomeView: UIView {
     
     // MARK: - Properties
-    var mainStack = Stack(axis: .vertical, distribution: .fill)
+    private var mainStack = Stack(axis: .vertical, distribution: .fill)
     
-    private var parametersStack = Stack(axis: .horizontal)
-    private var location1Stack = Stack(axis: .vertical)
+    private var location1Header = HeaderView(icon: .location, title: "Location 1")
+    private var location1Stack = Stack(axis: .horizontal)
     var lat1field = LocationTexfield(.latitude)
     var lon1field = LocationTexfield(.longitude)
-    private var location2Stack = Stack(axis: .vertical)
+    
+    private var location2Header = HeaderView(icon: .location, title: "Location 2")
+    private var location2Stack = Stack(axis: .horizontal)
     var lat2field = LocationTexfield(.latitude)
     var lon2field = LocationTexfield(.longitude)
     
+    var resultsHeader = HeaderView(icon: .results, title: "Results")
     var resultInfoLabel = Label(text: "Fill in all the fields", font: .systemFont(ofSize: 14, weight: .bold))
     
-    private var resultsDistanceStack = Stack(axis: .horizontal)
-    private var kmStack = Stack(axis: .vertical)
-    private var kmStaticLabel = Label(text: "km:", font: .systemFont(ofSize: 14, weight: .bold))
+    private var resultsStack = Stack(axis: .vertical)
+    private var kmStaticLabel = Label(text: "Distance in km:", font: .systemFont(ofSize: 14, weight: .bold))
     var kmLabel = Label(text: "-", font: .systemFont(ofSize: 14, weight: .regular))
-    private var mStaticLabel = Label(text: "m:", font: .systemFont(ofSize: 14, weight: .bold))
-    private var mStack = Stack(axis: .vertical)
+    private var mStaticLabel = Label(text: "Distance in m:", font: .systemFont(ofSize: 14, weight: .bold))
     var mLabel = Label(text: "-", font: .systemFont(ofSize: 14, weight: .regular))
-    
-    private var resultsLocationsStack = Stack(axis: .horizontal)
-
-    private var startLocationStack = Stack(axis: .vertical)
-    private var startStaticLabel = Label(text: "Start:", font: .systemFont(ofSize: 14, weight: .bold))
+    private var startStaticLabel = Label(text: "Location 2:", font: .systemFont(ofSize: 14, weight: .bold))
     var startLabel = Label(text: "-", font: .systemFont(ofSize: 14, weight: .regular))
-
-    private var destinationLocationStack = Stack(axis: .vertical)
-    private var destinationStaticLabel = Label(text: "Destination:", font: .systemFont(ofSize: 14, weight: .bold))
+    private var destinationStaticLabel = Label(text: "Location 2:", font: .systemFont(ofSize: 14, weight: .bold))
     var destinationLabel = Label(text: "-", font: .systemFont(ofSize: 14, weight: .regular))
     
     var getDistanceButton = ActionButton(title: "Get distance".uppercased())
     
-    var mapView: MKMapView = {
-        let map = MKMapView()
-        map.layer.cornerRadius = 16
-        return map
-    }()
-    
     // MARK: - Initializers
     init() {
         super.init(frame: .zero)
+        backgroundColor = .background
         addViews()
-      //  setSpacings()
     }
     
     required init?(coder: NSCoder) {
@@ -65,27 +54,21 @@ class HomeView: UIView {
     
     // MARK: - Layout
     func addViews() {
-        addSubviews(mainStack, mapView)
-        mainStack.addArrangedSubviews(resultInfoLabel, parametersStack, resultsDistanceStack, resultsLocationsStack, getDistanceButton)
-        parametersStack.addArrangedSubviews(location1Stack, location2Stack)
-        location1Stack.addArrangedSubviews(lon1field, lat1field)
-        location2Stack.addArrangedSubviews(lon2field, lat2field)
-        resultsDistanceStack.addArrangedSubviews(kmStack, mStack)
-        kmStack.addArrangedSubviews(kmStaticLabel, kmLabel)
-        mStack.addArrangedSubviews(mStaticLabel, mLabel)
-        resultsLocationsStack.addArrangedSubviews(startLocationStack, destinationLocationStack)
-        startLocationStack.addArrangedSubviews(startStaticLabel, startLabel)
-        destinationLocationStack.addArrangedSubviews(destinationStaticLabel, destinationLabel)
+        addSubviews(mainStack, getDistanceButton)
+        mainStack.addArrangedSubviews(location1Header, location1Stack, location2Header, location2Stack, resultsHeader, resultInfoLabel, resultsStack)
+        location1Stack.addArrangedSubviews(lat1field, lon1field)
+        location2Stack.addArrangedSubviews(lat2field, lon2field)
+        resultsStack.addArrangedSubviews(kmStaticLabel, kmLabel, mStaticLabel, mLabel, startStaticLabel, startLabel, destinationStaticLabel, destinationLabel)
     }
     
     func setSpacings() {
-        let spacing: CGFloat = 16
-        [mainStack, parametersStack, location1Stack, location2Stack].forEach { (stack) in
+        let spacing: CGFloat = 24
+        [mainStack, location1Stack, location2Stack].forEach { (stack) in
             stack.spacing = spacing
         }
-        [kmStack, mStack].forEach { (stack) in
-            stack.spacing = 0
-        }
+        resultsStack.spacing = 4
+        resultsStack.isLayoutMarginsRelativeArrangement = true
+        resultsStack.layoutMargins = UIEdgeInsets(top: -8, left: 0, bottom: 0, right: 0)
     }
     
     override func updateConstraints() {
@@ -96,23 +79,32 @@ class HomeView: UIView {
             make.leading.equalToSuperview().offset(offset)
             make.trailing.equalToSuperview().offset(-offset)
         }
-    
-        [location1Stack, location2Stack, kmStack, mStack, startLocationStack, destinationLocationStack].forEach { (stack) in
-            stack.arrangedSubviews.forEach { (subview) in
-                subview.snp.makeConstraints { (make) in
-                    make.width.equalTo(160)
-                }
+        
+        [location1Stack, location2Stack].forEach { (stack) in
+            stack.isLayoutMarginsRelativeArrangement = true
+            stack.layoutMargins = UIEdgeInsets(top: 4, left: 0, bottom: 4, right: 0)
+        }
+        
+        [location1Header, location2Header, resultsHeader].forEach { (header) in
+            header.snp.makeConstraints { (make) in
+                make.height.equalTo(40)
+                make.width.equalTo(150)
             }
         }
+        
+        [lat1field, lat2field, lon1field, lon2field].forEach { (textfield) in
+            textfield.snp.makeConstraints { (make) in
+                make.width.equalTo(140)
+            }
+        }
+        
         getDistanceButton.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom).offset(-offset)
             make.width.equalTo(200)
             make.height.equalTo(50)
         }
-
-        mapView.snp.makeConstraints { (make) in
-            make.top.equalTo(mainStack.snp.bottom).offset(offset)
-            make.bottom.leading.trailing.equalToSuperview()
-        }
+        
         setSpacings()
         super.updateConstraints()
     }
